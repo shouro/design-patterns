9999c0ff7ec1566bffd296838ffe44c5ee5e21d3
shouro <sourogts@gmail.com>
2020-02-28 06:58:35 +0600

    first commit

---
+++ observer/observer_push_push.py
@@ -0,0 +1,88 @@
+import abc
+import random
+import pprint
+import time
+
+
+class IObserver(metaclass=abc.ABCMeta):
+    abc.abstractmethod
+    def update(self):
+        raise NotImplementedError
+
+
+class IObservable(metaclass=abc.ABCMeta):
+    abc.abstractmethod
+    def add(self, IObserver):
+        raise NotImplementedError
+    
+    abc.abstractmethod
+    def remove(self, IObserver):
+        raise NotImplementedError
+
+    abc.abstractmethod
+    def notify(self):
+        raise NotImplementedError
+
+
+class weatherStation(IObservable):
+    def __init__(self):
+        self.state = {}
+        self.observer_list = []
+
+    def add(self, IObserver):
+        self.observer_list.append(IObserver)
+    
+    def remove(self, IObserver):
+        try:
+            self.observer_list.remove(IObserver)
+        except ValueError as ve:
+            print(ve)
+            print('failed to remove observer')
+    
+    def notify(self):
+        self._setState()
+        for observer in self.observer_list:
+            observer.update(self._getState())
+
+    def _getState(self):
+        return self.state
+    
+    def _setState(self):
+        temperature = [i for i in range(0, 101)]
+        humidity = [i for i in range(0, 101)]
+        presure = [i for i in range(1, 101)]
+        self.state = {
+            'temperature': random.choice(temperature),
+            'humidity': random.choice(humidity),
+            'presure': random.choice(presure),
+        }
+
+
+class weatherClient(IObserver):
+    def __init__(self):
+        self.data = {}
+    
+    def update(self, data):
+        self.data = data
+
+    def show(self):
+        print(self.data)
+
+
+def simulate():
+    station = weatherStation()
+    client_1 = weatherClient()
+    client_2 = weatherClient()
+    client_3 = weatherClient()
+    station.add(client_1)
+    station.add(client_2)
+    tick = 5
+    while(tick > 0 ):
+        station.notify()
+        client_1.show()
+        client_2.show()
+        time.sleep(2)
+        tick = tick - 1
+
+if __name__ == "__main__":
+    simulate()
---
+++ strategy/strategy.py
@@ -0,0 +1,77 @@
+import abc
+
+
+class InterfaceFly(metaclass=abc.ABCMeta):
+    abc.abstractmethod
+    def fly(self):
+        raise NotImplementedError
+
+
+class InterfaceQuack(metaclass=abc.ABCMeta):
+    abc.abstractmethod
+    def quack(self):
+        raise NotImplementedError
+
+
+class InterfaceDisplay(metaclass=abc.ABCMeta):
+    abc.abstractmethod
+    def display(self):
+        raise NotImplementedError
+
+class SimpleFly(InterfaceFly):
+    def fly(self):
+        print('simple fly')
+
+class SimpleQuack(InterfaceQuack):
+    def quack(self):
+        print('simple quack')
+
+class SimpleDisply(InterfaceDisplay):
+    def display(self):
+        print('simple display')
+
+class WildFly(InterfaceFly):
+    def fly(self):
+        print('wild fly')
+
+class WildQuack(InterfaceQuack):
+    def quack(self):
+        print('wild quack')
+
+class WildDisply(InterfaceDisplay):
+    def display(self):
+        print('wild display')
+
+class Duck():
+    def __init__(self, fly_strategy, quack_strategy, display_strategy):
+        self.fly_strategy = fly_strategy
+        self.quack_strategy = quack_strategy
+        self.display_strategy = display_strategy
+    
+    def fly(self):
+        self.fly_strategy.fly()
+    
+    def quack(self):
+        self.quack_strategy.quack()
+    
+    def display(self):
+        self.display_strategy.display()
+    
+    def demo(self):
+        self.fly()
+        self.quack()
+        self.display()
+
+
+if __name__ == "__main__":
+    sf = SimpleFly()
+    sq = SimpleQuack()
+    sd = SimpleDisply()
+    simple_duck = Duck(sf, sq, sd)
+    simple_duck.demo()
+
+    wf = WildFly()
+    wq = WildQuack()
+    wd = WildDisply()
+    wild_duck = Duck(wf, wq, wd)
+    wild_duck.demo()
